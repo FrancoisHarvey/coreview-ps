1d5f7d2936ca7c9cebd632ed96b0f99ea6cfd8c2
Pascal Breton <pascal.breton@msss.gouv.qc.ca>
2024-08-06 09:00:38 -0400

    extract relevant flow execution info

--- src/coreview-ps/Public/Get-CvCfFlowExecution.ps1
+++ src/coreview-ps/Public/Get-CvCfFlowExecution.ps1
@@ -6,13 +6,112 @@ function Get-CvCfFlowExecution {
 		[guid]$ExecutionId
 	)
 
-	$flowExecution = Invoke-CvCfRestMethod -Endpoint "api/executions/$([guid]::Empty)/${ExecutionId}"
-
-	if (-not $flowExecution) {
-		Write-ErrorMsg FlowExecutionNotFound $ExecutionId
+	process {
+		FetchFlowExecutionDetailsFromCoreFlow
+		ExtractGeneralFlowInfo
+		ExtractFlowStatus
+		ExtractFlowInput
+		ExtractFlowError
+		ExtractFlowOutput
 	}
 
-	# TODO: extract execution info
+	end {
+		return (ReturnFlowExecutionInfo)
+	}
 
-	return $flowExecution
+	begin {
+		$mutable = @{}
+
+		function FetchFlowExecutionDetailsFromCoreFlow {
+			$mutable.flowExecution = Invoke-CvCfRestMethod -Endpoint "api/executions/$([guid]::Empty)/${ExecutionId}"
+
+			if (-not $mutable.flowExecution) {
+				Write-ErrorMsg FlowExecutionNotFound $ExecutionId
+			}
+		}
+
+		function ExtractGeneralFlowInfo {
+			$mutable.executionDetails = @{
+				ExecutionId = $mutable.flowExecution.workflowExecutionId
+				FlowId      = $mutable.flowExecution.workflowId
+			}
+		}
+
+		function ExtractFlowStatus {
+			$mutable.executionDetails += @{
+				Status = $mutable.flowExecution.status
+			}
+		}
+
+		function ExtractFlowInput {
+			if (-not $mutable.flowExecution.workflowInput) {
+				return
+			}
+
+			$mutable.executionDetails += @{
+				Input = $mutable.flowExecution.workflowInput | Convert-JsonToHashtable
+			}
+		}
+
+		function ExtractFlowError {
+			if ($mutable.flowExecution.error -and $mutable.flowExecution.cause) {
+				ExtractGenericErrorDetails
+
+				if ($mutable.flowExecution.executionHistoryEvents) {
+					ExtractErrorCause
+				}
+			}
+		}
+
+		function ExtractGenericErrorDetails {
+			$mutable.executionDetails += @{
+				Error = @{
+					Message = $mutable.flowExecution.error
+					Cause   = $mutable.flowExecution.cause
+				}
+			}
+		}
+
+		function ExtractErrorCause {
+			$failedStep = $mutable.flowExecution.executionHistoryEvents | Where-Object { $_.cause } | Select-Object -First 1
+
+			$mutable.executionDetails.Error += @{
+				StepName = $failedStep.action.name
+				Details  = $failedStep.cause
+			}
+		}
+
+		function ExtractFlowOutput {
+			ExtractGenericFlowOutput
+
+			if ($mutable.flowExecution.executionHistoryEvents) {
+				ExtractOutputFromExecutionHistory
+			}
+		}
+
+		function ExtractGenericFlowOutput {
+			if (-not $mutable.flowExecution.workflowOutput) {
+				return
+			}
+
+			$mutable.executionDetails += @{
+				Output = $mutable.flowExecution.workflowOutput | Convert-JsonToHashtable
+			}
+		}
+
+		function ExtractOutputFromExecutionHistory {
+			$mutable.executionDetails += @{
+				ExecutionHistory = $mutable.flowExecution.executionHistoryEvents | Where-Object { $_.output -and $_.output.Contains('"response":[') } | ForEach-Object {
+					@{
+						StepName = $_.action.name
+						Output   = $_.output | Convert-JsonToHashtable | Select-Object -ExpandProperty response | Select-Object -First 1
+					}
+				}
+			}
+		}
+
+		function ReturnFlowExecutionInfo {
+			return $mutable.executionDetails
+		}
+	}
 }
